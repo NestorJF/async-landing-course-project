569961d5ce23f18081e90e98b4da9d1781f33708
Nestor Flores <nesjfgon@gmail.com>
2022-07-02 22:21:59 -0600

    Updated new files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # async-landing-course-project
+
+A landing page using Javascript Async Functions and API from the RapidAPI Hub. Final Project for the 'Curso de Asincronismo con JavaScript' Platzi Course.
+
+Made by: Néstor Flores
---
+++ src/assets/main.js
@@ -0,0 +1,44 @@
+const API = 'https://genius-song-lyrics1.p.rapidapi.com/artists/1421/albums?per_page=20&page=1'
+const content = null || document.getElementById('content');
+
+
+const options = {
+	method: 'GET',
+	headers: {
+        //TODO: Replace with your own API key
+		'X-RapidAPI-Key': '',
+		'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+	}
+};
+
+async function getAlbums() {
+    const response = await fetch(API, options);
+    const data = await response.json();
+    console.log(data);
+    return data
+}
+
+(async () => {
+    try{
+        const results = await getAlbums();
+        let view = `
+            ${results.response.albums.map(album => `
+                <div class="group relative">
+                    <div
+                        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                        <img src="${album.cover_art_url}" alt="${album.full_title}" class="w-full">
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                        <h3 class="text-sm text-gray-700">
+                        <span aria-hidden="true" class="absolute inset-0"></span>
+                        ${album.name}
+                        </h3>
+                    </div>
+                </div>
+            `).slice(0, 4).join('')}
+        `;
+        content.innerHTML = view;
+    }catch(error){
+        console.log(error);
+    }
+})();
